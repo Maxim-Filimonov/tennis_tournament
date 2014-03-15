8f6d364021d16b5b176117099a26b4819c3608fb
Maxim Filimonov <tpaktopsp@gmail.com>
2014-03-15 14:59:01 +1100

    Cleanup difference rule applicability code

--- lib/tournament/rules/difference_rule_applicability.rb
+++ lib/tournament/rules/difference_rule_applicability.rb
@@ -3,12 +3,24 @@ module Tournament
     module DifferenceRuleApplicability
       def applicable?
         values = scoreboard.scores.map {|_,v| v}
-        return false if values.inject(:+) < 6
-        highest_score = values.max
-        lame_scores = values.reject { |v| v == highest_score }
+        if all_scored_more_than_3?(values)
+          score_difference(values) == required_difference
+        else
+          false
+        end
+      end
+
+
+      private
+      def score_difference(score_values)
+        highest_score = score_values.max
+        lame_scores = score_values.reject { |v| v == highest_score }
         next_highest_score = lame_scores.empty? ? [highest_score] : lame_scores
-        score_difference = highest_score - next_highest_score.max
-        score_difference == required_difference
+        highest_score - next_highest_score.max
+      end
+
+      def all_scored_more_than_3?(score_values)
+        score_values.inject(:+) >= score_values.length * 3
       end
     end
   end
