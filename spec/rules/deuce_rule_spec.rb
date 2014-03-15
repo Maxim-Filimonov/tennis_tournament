1013a2c4835fa051ade39276c27d2cb8061c1805
Maxim Filimonov <tpaktopsp@gmail.com>
2014-03-15 15:52:37 +1100

    Sneaky do end in deuce rule spec

--- spec/rules/deuce_rule_spec.rb
+++ spec/rules/deuce_rule_spec.rb
@@ -2,9 +2,7 @@ require 'spec_helper'
 require 'tournament/rules/deuce_rule'
 
 describe Tournament::Rules::DeuceRule do
-  it_behaves_like 'tournament rule' do
-
-  end
+  it_behaves_like 'tournament rule'
 
   it 'is not applicable when at least one player scored less than 3' do
     rule = described_class.new(instance_double('Scoreboard', scores: {'p1' => 2, 'p2' => 2}))
